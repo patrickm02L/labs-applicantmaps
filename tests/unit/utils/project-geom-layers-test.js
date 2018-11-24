77d5cc9bbea4d937d10418fb9e5a31a31074e069
allthesignals <wilburnforce@gmail.com>
2018-11-23 19:25:40 -0500

    Comment out entirely

--- tests/unit/utils/project-geom-layers-test.js
+++ tests/unit/utils/project-geom-layers-test.js
@@ -1,10 +1,10 @@
-import projectGeomLayers from 'labs-applicant-maps/utils/project-geom-layers';
-import { module, skip } from 'qunit';
+// import projectGeomLayers from 'labs-applicant-maps/utils/project-geom-layers';
+// import { module, skip } from 'qunit';
 
-module('Unit | Utility | project-geom-layers', function() {
-  // Replace this with your real tests.
-  skip('it works', function(assert) {
-    const result = projectGeomLayers;
-    assert.ok(result);
-  });
-});
+// module('Unit | Utility | project-geom-layers', function() {
+//   // Replace this with your real tests.
+//   skip('it works', function(assert) {
+//     const result = projectGeomLayers;
+//     assert.ok(result);
+//   });
+// });
--- tests/unit/utils/unify-polygons-test.js
+++ tests/unit/utils/unify-polygons-test.js
@@ -1,31 +1,31 @@
-import unifyPolygons from 'labs-applicant-maps/utils/unify-polygons';
-import { module, skip } from 'qunit';
+// import unifyPolygons from 'labs-applicant-maps/utils/unify-polygons';
+// import { module, skip } from 'qunit';
 
-const EmptyFeatureCollection = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: null,
-    properties: {},
-  }],
-};
+// const EmptyFeatureCollection = {
+//   type: 'FeatureCollection',
+//   features: [{
+//     type: 'Feature',
+//     geometry: null,
+//     properties: {},
+//   }],
+// };
 
-module('Unit | Utility | unify-polygons', function() {
-  // Replace this with your real tests.
-  const dummyFeatureCollection = EmptyFeatureCollection;
-  const dummyPolygon = {
-    type: 'Polygon',
-    coordinates: [[0, 0], [0, 1], [1, 1], [1, 0]],
-  };
+// module('Unit | Utility | unify-polygons', function() {
+//   // Replace this with your real tests.
+//   const dummyFeatureCollection = EmptyFeatureCollection;
+//   const dummyPolygon = {
+//     type: 'Polygon',
+//     coordinates: [[0, 0], [0, 1], [1, 1], [1, 0]],
+//   };
 
-  dummyFeatureCollection.features[0].geometry = dummyPolygon;
-  dummyFeatureCollection.features.push({
-    type: 'Feature',
-    geometry: dummyPolygon,
-  });
+//   dummyFeatureCollection.features[0].geometry = dummyPolygon;
+//   dummyFeatureCollection.features.push({
+//     type: 'Feature',
+//     geometry: dummyPolygon,
+//   });
 
-  skip('it works', function(assert) {
-    const result = unifyPolygons(dummyFeatureCollection);
-    assert.ok(result);
-  });
-});
+//   skip('it works', function(assert) {
+//     const result = unifyPolygons(dummyFeatureCollection);
+//     assert.ok(result);
+//   });
+// });
