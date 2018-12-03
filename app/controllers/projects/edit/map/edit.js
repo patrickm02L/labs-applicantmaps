addc73136cee06d146e9d49538609d0c975056b7
Chris Whong <chris@whong.org>
2018-12-03 16:17:54 -0500

    try embedding pdf as tax map

--- app/controllers/projects/edit/map/edit.js
+++ app/controllers/projects/edit/map/edit.js
@@ -2,5 +2,7 @@ import Controller from '@ember/controller';
 import areaMapLegendConfig from '../../../../utils/area-map-legend-config';
 
 export default class NewProjectMapController extends Controller {
+  queryParams = ['mapType'];
+
   areaMapLegendConfig = areaMapLegendConfig;
 }
--- app/models/tax-map.js
+++ app/models/tax-map.js
@@ -1,3 +1,7 @@
+import { attr } from '@ember-decorators/data';
 import ApplicantMap from './applicant-map';
 
-export default class extends ApplicantMap {}
+export default class extends ApplicantMap {
+  @attr('string', { defaultValue: 'Tax Map' })
+  mapTypeLabel;
+}
